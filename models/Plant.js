9f64c5b909c475a48abce0f077585a4165c81d12
Luis Silva <biokiller134@gmail.com>
2025-05-26 00:20:03 +0100

    Plant schema update

--- models/Plant.js
+++ models/Plant.js
@@ -1,5 +1,27 @@
 import mongoose from 'mongoose';
 
+const EventSchema = new mongoose.Schema({
+  event: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: new Date(),
+  }
+})
+
+const PestSchema = new mongoose.Schema({
+  pest_designation: {
+    type: String,
+    required: true,
+  },
+  treatment: {
+    type: String,
+    required: true,
+  }
+})
+
 const PlantSchema = new mongoose.Schema({
   designation: {
     type: String,
@@ -90,8 +112,28 @@ const PlantSchema = new mongoose.Schema({
     default: "",
   },
   observations: {
-    type: [String],
+    type: String,
     default: "",
+  },
+  necessities: {
+    type: [String],
+    default: [],
+  },
+  companion_plants: {
+    type: [String],
+    default: [],
+  },
+  antagonistic_plants: {
+    type: [String],
+    default: [],
+  },
+  pests: {
+    type: [PestSchema],
+    default: [],
+  },
+  events: {
+    type: [EventSchema],
+    default: [],
   }
 });
 
